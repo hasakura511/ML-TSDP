a4b44d14e2ec7260efbad8e1c6659380e7f9612d
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-01 19:59:43 +0900

    hi

--- ibapi/get_hist.py
+++ ibapi/get_hist.py
@@ -1,18 +1,18 @@
+import numpy as np
+import pandas as pd
 from datetime import datetime
 from threading import Event
 
 from swigibpy import EWrapper, EPosixClientSocket, Contract
-
-
-WAIT_TIME = 30.0
+WAIT_TIME = 30.0;
+p_date=[];
 p_open=[];
 p_high=[];
 p_low=[];
 p_close=[];
 p_volume=[];
 p_chg=[];
-
-###
+p_chgpt=[];
 
 
 class HistoricalDataExample(EWrapper):
@@ -56,63 +56,89 @@ class HistoricalDataExample(EWrapper):
 	    if len(p_close) > 0:
 	    	chgpt=close-p_close[-1];
 		chg=chgpt/p_close[-1];
-		
+	    p_date.append(date);		
             p_open.append(open);
             p_high.append(high);
             p_low.append(low);
             p_close.append(close);
             p_volume.append(volume);
+	    p_chgpt.append(chgpt);
+	    p_chg.append(chg);
 	    #date = datetime.strptime(date, "%Y%m%d").strftime("%d %b %Y")
             print(("History %s - Open: %s, High: %s, Low: %s, Close: "
                    "%s, Volume: %d, Change: %s, Net: %s") % (date, open, high, low, close, volume, chgpt, chg));
 
+def getHist(symbol, currency,client_id):
 
-# Instantiate our callback object
-callback = HistoricalDataExample()
+	# Instantiate our callback object
+	callback = HistoricalDataExample()
 
-# Instantiate a socket object, allowing us to call TWS directly. Pass our
-# callback object so TWS can respond.
+	# Instantiate a socket object, allowing us to call TWS directly. Pass our
+	# callback object so TWS can respond.
 
-tws = EPosixClientSocket(callback)
-#tws = EPosixClientSocket(callback, reconnect_auto=True)
-# Connect to tws running on localhost
-if not tws.eConnect("", 7496, 111):
-    raise RuntimeError('Failed to connect to TWS')
+	tws = EPosixClientSocket(callback)
+	#tws = EPosixClientSocket(callback, reconnect_auto=True)
+	# Connect to tws running on localhost
+	if not tws.eConnect("", 7496, client_id):
+	    raise RuntimeError('Failed to connect to TWS')
+	# Simple contract for GOOG
+	contract = Contract()
+	contract.exchange = "IDEALPRO"
+	contract.symbol = symbol
+	contract.secType = "CASH"
+	contract.currency = currency
+	today = datetime.today()
 
-# Simple contract for GOOG
-contract = Contract()
-contract.exchange = "IDEALPRO"
-contract.symbol = "USD"
-contract.secType = "CASH"
-contract.currency = "JPY"
-today = datetime.today()
+	print("Requesting historical data for %s" % contract.symbol)
 
-print("Requesting historical data for %s" % contract.symbol)
+	# Request some historical data.
+	tws.reqHistoricalData(
+    		1,                                         # tickerId,
+    		contract,                                   # contract,
+    		today.strftime("%Y%m%d %H:%M:%S %Z"),       # endDateTime,
+    		"12 M",                                      # durationStr,
+    		"1 day",                                    # barSizeSetting,
+    		"ASK",                                   # whatToShow,
+    		1,                                          # useRTH,
+    		1                                          # formatDate
+	)
 
-# Request some historical data.
-tws.reqHistoricalData(
-    1,                                         # tickerId,
-    contract,                                   # contract,
-    today.strftime("%Y%m%d %H:%M:%S %Z"),       # endDateTime,
-    "1 W",                                      # durationStr,
-    "1 day",                                    # barSizeSetting,
-    "ASK",                                   # whatToShow,
-    1,                                          # useRTH,
-    1                                          # formatDate
-)
-
-print("\n====================================================================")
-print(" History requested, waiting %ds for TWS responses" % WAIT_TIME)
-print("====================================================================\n")
+	print("\n====================================================================")
+	print(" History requested, waiting %ds for TWS responses" % WAIT_TIME)
+	print("====================================================================\n")
 
 
-try:
-    callback.got_history.wait(timeout=WAIT_TIME)
-except KeyboardInterrupt:
-    pass
-finally:
-    if not callback.got_history.is_set():
-        print('Failed to get history within %d seconds' % WAIT_TIME)
+	try:
+    		callback.got_history.wait(timeout=WAIT_TIME)
+	except KeyboardInterrupt:
+    		pass
+	finally:
+    		if not callback.got_history.is_set():
+        		print('Failed to get history within %d seconds' % WAIT_TIME)
 
-    print("\nDisconnecting...")
-    tws.eDisconnect()
+    	print("\nDisconnecting...")
+
+	#dataSet['Open']=p_open;
+	#dataSet['High']=p_high;
+	#dataSet['Low']=p_low;
+	#dataSet['Close']=p_close;
+	#dataSet['Volume']=p_volume;
+	#dataSet['Change']=p_chg;
+	#dataSet['Net']=p_chgpt;
+	filename_df=symbol + currency;
+	#dataSet = pd.concat([qt[' OPEN'],qt[' HIGH'],qt[' LOW'],qt[' CLOSE'],qt[' VOL'],qt[' CHANGE'],qt[' NET']], axis=1)
+	d={'Date':p_date, 'Open':p_open, 'High':p_high, 'Low':p_low, 'Close':p_close, 'Volume':p_volume, 'Change':p_chgpt, 'Net':p_chg}
+	i = ['Date','Open','High','Low','Close','Volume','Change','Net']
+	dataSet=pd.DataFrame(data=d, columns=i);
+
+	#dataSet.to_csv('/media/sf_Python/data/OHLCV_'+filename_df, index=False)
+	dataSet.to_csv('./OHLCV_'+filename_df+'.csv', index=False)
+
+
+
+
+getHist('USD','JPY',111)
+getHist('EUR','USD',112)
+getHist('USD','CAD',113)
+
+#tws.eDisconnect()

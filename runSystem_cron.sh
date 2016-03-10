7dad2e74fc42609a6f69a9ef20b49341cc5a152e
U-CONDOR2298\Administrator <Administrator@condor2298.startdedicated.com>
2016-03-10 00:35:10 -0600

    hi

--- runSystem_cron.sh
+++ runSystem_cron.sh
@@ -1,47 +1,29 @@
 #!/bin/sh
-export PATH="/usr/local/bin:/usr/local/sbin:~/Documents/Android/sdk/platform-tools:~/Documents/Android/sdk/tools:~/Documents/Android/android-ndk-r10e:$PATH"
 
-export LDFLAGS="-L/usr/local/opt/tcl-tk/lib:$LDFLAGS"
-export CPPFLAGS="-I/usr/local/opt/tcl-tk/include:$CPPFLAGS"
-export TK_LIBRARY="/usr/local/Cellar/tcl-tk/8.6.4/lib"
-export LD_LIBRARY_PATH="/usr/local/opt/tcl-tk/lib:$LD_LIBRARY_PATH"
+cd /media/sf_Python/TSDP/
+/anaconda2/python get_ibpos.py
+/anaconda2/python system_EURJPY.py 1
+/anaconda2/python system_EURUSD.py 1
+/anaconda2/python system_GBPUSD.py 1
+/anaconda2/python system_USDJPY.py 1
+/anaconda2/python system_USDCHF.py 1
+/anaconda2/python system_AUDUSD.py 1
+/anaconda2/python system_USDCAD.py 1
+/anaconda2/python proc_signal_v2.py
 
-export MONO_GAC_PREFIX="/usr/local"
-#export DYLD_LIBRARY_PATH="/usr/local/Cellar/openssl/1.0.2d/lib"
-export DYLD_FALLBACK_LIBRARY_PATH="$HOME/anaconda/lib/:$DYLD_FALLBACK_LIBRARY_PATH"
-# added by Anaconda 2.3.0 installer
-export PATH="/Users/admin/anaconda/bin:$PATH"
+/anaconda2/python get_ibpos.py
 
-# added by Anaconda 2.3.0 installer
-export PATH="/Users/admin/anaconda/bin:$PATH"
-
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-cd /Users/admin/ML-TSDP/
-/Users/admin/anaconda/bin/python get_ibpos.py
-/Users/admin/anaconda/bin/python system_EURJPY.py 1
-/Users/admin/anaconda/bin/python system_EURUSD.py 1
-/Users/admin/anaconda/bin/python system_GBPUSD.py 1
-/Users/admin/anaconda/bin/python system_USDJPY.py 1
-/Users/admin/anaconda/bin/python system_USDCHF.py 1
-/Users/admin/anaconda/bin/python system_AUDUSD.py 1
-/Users/admin/anaconda/bin/python system_USDCAD.py 1
-/Users/admin/anaconda/bin/python proc_signal_v2.py
-
-/Users/admin/anaconda/bin/python get_ibpos.py
-
-/Users/admin/anaconda/bin/python system_AUDUSD_v2.1.py 1
-/Users/admin/anaconda/bin/python proc_signal_v2dps.py
-#/Users/admin/anaconda/bin/python get_exec.py
+/anaconda2/python system_AUDUSD_v2.1.py 1
+/anaconda2/python proc_signal_v2dps.py
+#/anaconda2/python get_exec.py
 sleep 10
-/Users/admin/anaconda/bin/python get_ibpos.py
+/anaconda2/python get_ibpos.py
 sleep 10
-/Users/admin/anaconda/bin/python proc_pos_adj.py	
+/anaconda2/python proc_pos_adj.py	
 
-/Users/admin/anaconda/bin/python get_ibpos.py
+/anaconda2/python get_ibpos.py
 
 
-/Users/admin/anaconda/bin/python system_BTCUSD_bitstamp_hourly.py 1
-/Users/admin/anaconda/bin/python system_BTCUSD_bitstamp.py 1
-#/Users/admin/anaconda/bin/python get_exec.py
+/anaconda2/python system_BTCUSD_bitstamp_hourly.py 1
+/anaconda2/python system_BTCUSD_bitstamp.py 1
+#/anaconda2/python get_exec.py

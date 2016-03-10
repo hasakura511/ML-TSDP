f852c4203c740b0236b6c0e35b8f3793c44880ae
U-CONDOR2298\Administrator <Administrator@condor2298.startdedicated.com>
2016-03-10 08:35:53 -0600

    hi

--- incr_counter.py
+++ incr_counter.py
@@ -7,4 +7,5 @@ data['counter']=counter
 data=data.set_index('counter')
 data.to_csv('./data/counter')
 if int(counter) >= 7:
+	subprocess.call(['python', 'get_ibpos.py'])
 	subprocess.call(['python', 'proc_signal_v2dps.py'])	

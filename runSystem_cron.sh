0e9365582bcc33bdd7eddc409c5db242178ebaa8
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-10 21:11:47 +0900

    hi

--- runSystem_cron.sh
+++ runSystem_cron.sh
@@ -3,14 +3,13 @@ cd /media/sf_Python/TSDP/
 /anaconda2/python get_ibpos.py
 
 /anaconda2/python/v2_get_all_pairs.py
-/anaconda2/python system_EURJPY_v2.2C.py 1
-/anaconda2/python system_EURUSD_v2.2C.py 1
-/anaconda2/python system_GBPUSD_v2.2C.py 1
-/anaconda2/python system_USDJPY_v2.2C.py 1
-/anaconda2/python system_USDCHF_v2.2C.py 1
-/anaconda2/python system_AUDUSD_v2.2C.py 1
-/anaconda2/python system_USDCAD_v2.2C.py 1
-/anaconda2/python proc_signal_v2dps.py
+/anaconda2/python system_EURJPY_v2.2C.py 1 &
+/anaconda2/python system_EURUSD_v2.2C.py 1 &
+/anaconda2/python system_GBPUSD_v2.2C.py 1 &
+/anaconda2/python system_USDJPY_v2.2C.py 1 &
+/anaconda2/python system_USDCHF_v2.2C.py 1 &
+/anaconda2/python system_AUDUSD_v2.2C.py 1 &
+/anaconda2/python system_USDCAD_v2.2C.py 1 &
 
 /anaconda2/python system_EURJPY.py 1
 /anaconda2/python system_EURUSD.py 1
@@ -24,5 +23,9 @@ cd /media/sf_Python/TSDP/
 
 sleep 10
 /anaconda2/python proc_pos_adj.py	
+
+sleep 360
+/anaconda2/python proc_signal_v2dps.py
+
 /anaconda2/python system_BTCUSD_bitstamp_hourly.py 1
 /anaconda2/python system_BTCUSD_bitstamp.py 1
--- start_usdjpy.cmd
+++ start_usdjpy.cmd
@@ -1,3 +1,4 @@
 cd \cygwin64\media\sf_python\tsdp
-\anaconda2\python system_USDJPY_v2.2C.py 1 > \logs\USDJPYv2.log 2>&1
+\anaconda2\python system_USDJPY_v2.2C.py 1 >> \logs\USDJPYv2.log 2>&1
+\anaconda2\python proc_signal_v2dps.py
 exit

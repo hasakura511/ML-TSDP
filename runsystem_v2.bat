f72ef98d5350e7032c1aeda9e38cacd63a53b6fd
U-CONDOR2298\Administrator <Administrator@condor2298.startdedicated.com>
2016-03-10 07:17:35 -0600

    hi

---
+++ runsystem_v2.bat
@@ -0,0 +1,9 @@
+cd \cygwin64\media\sf_python\tsdp
+\anaconda2\python get_ibpos.py
+start /b .\start_eurjpy.cmd
+start /b .\start_eurusd.cmd
+start /b .\start_gbpusd.cmd
+start /b .\start_usdjpy.cmd
+start /b .\start_usdchf.cmd
+start /b .\start_audusd.cmd
+start /b .\start_usdcad.cmd

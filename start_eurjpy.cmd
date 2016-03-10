fcad74fb8fb2dcd2b17a78dba6eba568f5b56ff5
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-10 22:21:10 +0900

    hi

--- start_eurjpy.cmd
+++ start_eurjpy.cmd
@@ -1,4 +1,4 @@
 cd \cygwin64\media\sf_python\tsdp
 \anaconda2\python system_EURJPY_v2.2C.py 1 >> \logs\eurjpyv2.log 2>&1
-\anaconda2\python proc_signal_v2dps.py
+\anaconda2\python incr_counter.py
 exit
--- start_eurusd.cmd
+++ start_eurusd.cmd
@@ -1,4 +1,4 @@
 cd \cygwin64\media\sf_python\tsdp
 \anaconda2\python system_EURUSD_v2.2C.py 1 >> \logs\EURUSDv2.log 2>&1
-\anaconda2\python proc_signal_v2dps.py
+\anaconda2\python incr_counter.py
 exit

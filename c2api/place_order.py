f4af8f6f2e24023b292013effec04192d6f15a4e
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-11 19:13:17 +0900

    hi

--- c2api/place_order.py
+++ c2api/place_order.py
@@ -28,4 +28,34 @@ def place_order(action, quant, sym, type, systemid, submit,apikey):
     sleep(2)
     print r.text
 
+def set_position(positions, systemid, submit, apikey):
+    if submit == False:
+        return 0;
+    url = 'https://api.collective2.com/world/apiv3/setDesiredPositions'
+    
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    
+    data = { 
+          "apikey":   apikey, # "tXFaL4E6apdfmLtGasIovtGnUDXH_CQso7uBpOCUDYGVcm1w0w", 
+          "systemid": systemid, 
+          "positions": positions
+          #{
+          #   "symbol" : "MSFT",
+          #   "typeofsymbol" : "stock",
+          #   "quant" : -30
+          #},
+          # {
+          #    "symbol" : "@ESH6",
+          #    "typeofsymbol" : "future",
+          #   "quant" : 1
+          # }
+          #
+          #
+    }
+    
+    params={}
+    
+    r=requests.post(url, params=params, json=data);
+    sleep(2)
+    print r.text
 #place_order('BTO','1','EURUSD','forex')

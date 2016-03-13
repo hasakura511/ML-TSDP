631dc4801256d3e9a4ce5278d46bec441f7cde0a
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-13 21:39:26 +0900

    hi

--- start_audusd.cmd
+++ start_audusd.cmd
@@ -1,4 +1,4 @@
 cd \cygwin64\media\sf_python\tsdp
-\anaconda2\python system_2.3C.py AUDUSD >> \logs\AUDUSDv2.log 2>&1
+\anaconda2\python system_v2.3C.py AUDUSD >> \logs\AUDUSDv2.log 2>&1
 \anaconda2\python incr_counter.py
 exit
--- start_eurusd.cmd
+++ start_eurusd.cmd
@@ -1,4 +1,4 @@
 cd \cygwin64\media\sf_python\tsdp
-\anaconda2\python system_2.3C.py EURUSD >> \logs\EURUSDv2.log 2>&1
+\anaconda2\python system_v2.3C.py EURUSD >> \logs\EURUSDv2.log 2>&1
 \anaconda2\python incr_counter.py
 exit

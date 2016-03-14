fa9dc313368f6b544a77a83b369493b7530b71a2
U-CONDOR2298\Administrator <Administrator@condor2298.startdedicated.com>
2016-03-14 00:57:31 -0400

    hi

--- runsystem_cron_min.bat
+++ runsystem_cron_min.bat
@@ -1,7 +1,7 @@
 cd \cygwin64\media\sf_python\tsdp
 
 
-\anaconda2\python system_v1.py 1
+\anaconda2\python system_v1.1.py 1
 \anaconda2\python get_ibpos.py
 \anaconda2\python proc_signal.py
 \anaconda2\python get_ibpos.py

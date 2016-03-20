4f59a9f7d03ebe7454f38ec10e84efa8b020d45d
hasakura511 <hidemi@gmail.com>
2016-03-20 21:02:20 +0900

    new pair start files

---
+++ start_cadjpy_v3.cmd
@@ -0,0 +1,4 @@
+cd \cygwin64\media\sf_python\tsdp
+\anaconda2\python system_v3.0C.py CADJPY 1 >> \logs\CADJPYv3.log 2>&1
+\anaconda2\python incr_counter.py
+exit
---
+++ start_nzdjpy_v2.cmd
@@ -0,0 +1,4 @@
+cd \cygwin64\media\sf_python\tsdp
+\anaconda2\python system_v2.31C.py NZDJPY 1 >> \logs\NZDJPYv2.log 2>&1
+\anaconda2\python incr_counter.py
+exit

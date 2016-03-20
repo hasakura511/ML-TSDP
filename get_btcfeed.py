4a500381c017f867b132f86c7cf5d438c0e990c8
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-20 21:43:09 +0900

    hi

--- get_btcfeed.py
+++ get_btcfeed.py
@@ -15,6 +15,13 @@ from btapi.get_signal import get_v1signal
 from btapi.get_hist_btcharts import get_bthist
 from btapi.raw_to_ohlc import feed_to_ohlc, feed_ohlc_to_csv
 from seitoolz.paper import adj_size
+import sys
+import pusherclient #live stream client: https://github.com/ekulyk/PythonPusherClient
+import logging
+import time
+import websocket
+
+logging.basicConfig()
 
 debug=True
 
@@ -31,10 +38,18 @@ commissiondata['key']=commissiondata['Symbol']  + commissiondata['Currency'] + c
 commissiondata=commissiondata.set_index('key')
 
 def get_btc_ask(ticker, exchange):
-    return feed[exchange]['price']
+    if exchange == 'bitstampUSD':
+        #print 'bitstmp ask' + str(feed[exchange]['ask'])
+        return feed[exchange]['ask']
+    else:
+        return feed[exchange]['price']
 
 def get_btc_bid(ticker, exchange):
-    return feed[exchange]['price']
+    if exchange == 'bitstampUSD':
+        #print 'bitstmp bid' + str(feed[exchange]['bid'])
+        return feed[exchange]['bid']
+    else:
+        return feed[exchange]['price']
 
 def get_btcfeed():
     global feed
@@ -70,11 +85,72 @@ def get_btcfeed():
                 exchange=dataSet['symbol']
                 feedid=dataSet['id']
                 #print "Price: " + str(price)
-                feed[exchange]=dataSet
-                feed_to_ohlc('BTCUSD',exchange, price, timestamp, vol)
+                if exchange != 'bitstampUSD':
+                    feed[exchange]=dataSet
+                    feed_to_ohlc('BTCUSD',exchange, price, timestamp, vol)
                 
     return
 
+def bitstamp_order_book_callback(data):
+    #print "book", data
+    jsondata = json.loads(data)
+    dataSet=json_normalize(jsondata).iloc[-1]
+    if not feed.has_key('bitstampUSD'):
+        feed['bitstampUSD']=dict()
+        feed['bitstampUSD']['bid']=-1
+        feed['bitstampUSD']['ask']=-1
+        
+    if 'bids' in dataSet:
+        bids=dataSet['bids']
+        data=list()
+        for bid in bids:
+            data.append(bid[0])
+        feed['bitstampUSD']['bid']=float(max(data))
+    if 'asks' in dataSet:
+        asks=dataSet['asks']
+        data=list()
+        for ask in asks:
+            data.append(ask[0])
+        feed['bitstampUSD']['ask']=float(min(data))    
+    
+def bitstamp_trade_callback(data):
+    #trade {"price": 408.80000000000001, "amount": 0.076399999999999996, "id": 10832011}
+    jsondata = json.loads(data)
+    dataSet=json_normalize(jsondata).iloc[-1]
+    eastern=timezone('US/Eastern')
+    endDateTime=dt.now(get_localzone())
+    date=endDateTime.astimezone(eastern)
+    timestamp=int(date.strftime("%s"))
+    vol=float(dataSet['amount'])
+    price=float(dataSet['price'])
+    exchange='bitstampUSD'
+    feedid=dataSet['id']
+    #print exchange + str(price) + ' ' + str(timestamp) + ' ' + str(vol)
+    #print "Price: " + str(price)
+    #feed[exchange]=dataSet
+    feed_to_ohlc('BTCUSD',exchange, float(price), int(timestamp), float(vol))
+    #print "trade", data
+    
+    
+def bitstamp_connect_handler(data): #this gets called when the Pusher connection is established
+    trades_channel = pusher.subscribe("live_trades")
+    trades_channel.bind('trade', bitstamp_trade_callback)
+
+    order_book_channel = pusher.subscribe('order_book');
+    order_book_channel.bind('data', bitstamp_order_book_callback)
+
+    #orders_channel = pusher.subscribe("live_orders")
+    #orders_channel.bind('order_deleted', order_deleted_callback)
+    #orders_channel.bind('order_created', order_created_callback)
+    #orders_channel.bind('order_changed', order_changed_callback)
+    
+def get_bitstampfeed():
+    global pusher
+    pusher = pusherclient.Pusher("de504dc5763aeef9ff52")
+    pusher.connection.logger.setLevel(logging.WARNING) #no need for this line if you want everything printed out by the logger
+    pusher.connection.bind('pusher:connection_established', bitstamp_connect_handler)
+    pusher.connect()
+    
 def get_signal():
     global feed
     global ohlc
@@ -84,7 +160,8 @@ def get_signal():
       myfeed=feed.copy()
       for exchange in myfeed:
             
-         ticker='BTCUSD'
+        ticker='BTCUSD'
+        if get_btc_ask(ticker, exchange) > 0 and get_btc_bid(ticker, exchange) > 0:
          data=get_ohlc(ticker, exchange)
          if data.shape[0] > 2000:
             #model=get_v1signal(data.tail(2000), ticker, exchange)
@@ -94,7 +171,8 @@ def get_signal():
                 if system['System'] == 'BTCUSD':
                     system['Name']=system['Name'] + '_' + exchange
                     system['System'] = ticker + '_' + exchange
-                
+                    
+                  
                     ask=float(get_btc_ask(ticker, exchange))
                     bid=float(get_btc_bid(ticker, exchange))
                     secType=system['ibtype']
@@ -123,7 +201,7 @@ def get_signal():
                         adj_size(model, system['System'],system['Name'],pricefeed,\
                         str(system['c2id']),system['c2api'],system['c2qty'],system['c2sym'],system['c2type'], system['c2submit'], \
                             system['ibqty'],system['ibsym'],system['ibcur'],system['ibexch'],system['ibtype'],system['ibsubmit'], date)
-                    time.sleep(120)
+                    time.sleep(1)
 
 def get_ohlc(ticker, exchange):
     global feed
@@ -145,6 +223,7 @@ signal_thread.daemon=True
 threads.append(feed_thread)
 threads.append(signal_thread)
 
+get_bitstampfeed()
 feed_thread.start()
 get_signal()
  

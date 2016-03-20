67aa026e954f59b47acbd8e1ff55b176a78711e0
hasakura511 <hidemi@gmail.com>
2016-03-20 18:58:29 +0900

    rem v3until new server

--- runsystem_cron.bat
+++ runsystem_cron.bat
@@ -2,13 +2,13 @@ cd \cygwin64\media\sf_python\tsdp
 
 \anaconda2\python make_counter.py
 
-start /b .\start_eurjpy_v3.cmd
-start /b .\start_eurusd_v3.cmd
-start /b .\start_gbpusd_v3.cmd
-start /b .\start_usdjpy_v3.cmd
-start /b .\start_usdchf_v3.cmd
-start /b .\start_audusd_v3.cmd
-start /b .\start_usdcad_v3.cmd
+rem start /b .\start_eurjpy_v3.cmd
+rem start /b .\start_eurusd_v3.cmd
+rem start /b .\start_gbpusd_v3.cmd
+rem start /b .\start_usdjpy_v3.cmd
+rem start /b .\start_usdchf_v3.cmd
+rem start /b .\start_audusd_v3.cmd
+rem start /b .\start_usdcad_v3.cmd
 
 start /b .\start_eurjpy_v2.cmd
 start /b .\start_eurusd_v2.cmd

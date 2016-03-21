c4af5ef33dbda18e327eadeaf2d4a98e5a2a5b30
yfushimi@neospace.com <yfushimi@neospace.com>
2016-03-21 20:56:40 +0900

    hi

--- runsystem_cron.bat
+++ runsystem_cron.bat
@@ -18,19 +18,19 @@ rem start /b .\start_gbpjpy_v3.cmd
 rem start /b .\start_nzdjpy_v3.cmd
 rem start /b .\start_nzdusd_v3.cmd
 
-start /b .\start_eurjpy_v2.cmd
-start /b .\start_eurusd_v2.cmd
-start /b .\start_gbpusd_v2.cmd
-start /b .\start_usdjpy_v2.cmd
-start /b .\start_usdchf_v2.cmd
-start /b .\start_audusd_v2.cmd
-start /b .\start_usdcad_v2.cmd
-start /b .\start_audjpy_v2.cmd
-start /b .\start_cadjpy_v2.cmd
-start /b .\start_chfjpy_v2.cmd
-start /b .\start_eurchf_v2.cmd
-start /b .\start_eurgbp_v2.cmd
-start /b .\start_gbpjpy_v2.cmd
-start /b .\start_nzdjpy_v2.cmd
-start /b .\start_nzdusd_v2.cmd
+rem start /b .\start_eurjpy_v2.cmd
+rem start /b .\start_eurusd_v2.cmd
+rem start /b .\start_gbpusd_v2.cmd
+rem start /b .\start_usdjpy_v2.cmd
+rem start /b .\start_usdchf_v2.cmd
+rem start /b .\start_audusd_v2.cmd
+rem start /b .\start_usdcad_v2.cmd
+rem start /b .\start_audjpy_v2.cmd
+rem start /b .\start_cadjpy_v2.cmd
+rem start /b .\start_chfjpy_v2.cmd
+rem start /b .\start_eurchf_v2.cmd
+rem start /b .\start_eurgbp_v2.cmd
+rem start /b .\start_gbpjpy_v2.cmd
+rem start /b .\start_nzdjpy_v2.cmd
+rem start /b .\start_nzdusd_v2.cmd
 

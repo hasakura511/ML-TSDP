60ab97feef7683778f3b03275b6621ca7d4c68f1
hasakura511 <hidemi@gmail.com>
2016-04-02 15:12:13 +0900

    update

--- runsystem_onBar.py
+++ runsystem_onBar.py
@@ -15,21 +15,65 @@ from btapi.get_signal import get_v1signal
 from btapi.get_hist_btcharts import get_bthist
 from btapi.raw_to_ohlc import feed_to_ohlc, feed_ohlc_to_csv
 from seitoolz.paper import adj_size
+from forecast import runv2
 import sys
 import pusherclient #live stream client: https://github.com/ekulyk/PythonPusherClient
 import logging
 import time
 import websocket
+from suztoolz.display import offlineMode
 
-logging.basicConfig(filename='/logs/runsystem_v1.log',level=logging.DEBUG)
+logging.basicConfig(filename='/logs/runsystem_v1v2.log',level=logging.DEBUG)
 
 debug=False
+
+filterName = 'DF1'
+data_type = 'ALL'
+barSizeSetting='30m'
 
 pairs=['NZDJPY','CADJPY','CHFJPY','EURGBP',\
                  'GBPJPY','EURCHF','AUDJPY',\
                  'AUDUSD','EURUSD','GBPUSD','USDCAD',\
                  'USDCHF','USDJPY','EURJPY','NZDUSD']
-                
+                 
+livePairs =  [
+                #'NZDJPY',\
+                #'CADJPY',\
+                #'CHFJPY',\
+                #'EURJPY',\
+                #'GBPJPY',\
+                #'AUDJPY',\
+                #'USDJPY',\
+                'AUDUSD',\
+                #'EURUSD',\
+                #'GBPUSD',\
+                #'USDCAD',\
+                #'USDCHF',\
+                #'NZDUSD',
+                #'EURCHF',\
+                #'EURGBP'\
+                ]
+#settings
+showDist =  False
+showPDFCDF = False
+showAllCharts = False
+perturbData = False
+runDPS = True
+saveParams = False
+saveDataSet=True
+verbose= False
+#paths
+scorePath = None
+equityStatsSavePath = None
+signalPath = '../data/signals/'
+dataPath = '../data/from_IB/'
+bestParamsPath =  '../data/params/'
+chartSavePath = '../data/results/' 
+        
+
+            
+
+                                   
 def get_bars(pairs, interval):
     #global SST
     mypairs=list()
@@ -41,17 +85,32 @@ def onBar(bar, symbols):
     #global SST
     #SST = SST.combine_first(bar).sort_index()
     for sym in symbols:
-        runPair(sym)
+        runPair_v1(sym)
+        runPair_v2(sym)
         
-def runPair(pair):
+def runPair_v1(pair):
+    version = 'v1'
+    version_ = 'v1.3C'
+    runDPS = False
+    runData = {'ticker':pair, 'showDist':showDist,'showPDFCDF':showPDFCDF,'showAllCharts':showAllCharts,\
+                'runDPS':runDPS,'saveParams':saveParams,'saveDataSet':saveDataSet,'verbose':verbose,\
+                'scorePath' : scorePath, 'equityStatsSavePath' : equityStatsSavePath,'signalPath' : signalPath,\
+                'dataPath' :dataPath, 'bestParamsPath' :  bestParamsPath, 'chartSavePath' :chartSavePath,\
+                'version':version, 'version_':version_, 'filterName':filterName, 'data_type':data_type,\
+                'barSizeSetting':barSizeSetting}
+                
+    if pair not in livePairs:
+        offlineMode(pair, "Offline Mode: turned off in runsystem", signalPath, version, version_)
+        
     try:
-            f=open ('/logs/' + pair + 'onBar.log','a')
-            print 'Starting V1: ' + pair
-            f.write('Starting V1: ' + pair)
+            f=open ('/logs/' + version+'_'+pair + 'onBar.log','a')
+            print 'Starting '+version+': ' + pair
+            f.write('Starting '+version+': ' + pair)
             
-            ferr=open ('/logs/' + pair + 'onBar_err.log','a')
-            ferr.write('Starting V1: ' + pair)
+            ferr=open ('/logs/' + version+'_'+pair + 'onBar_err.log','a')
+            ferr.write('Starting '+version+': ' + pair)
             
+            signal=runv2(runData)
             subprocess.call(['python','debug_system_v1.3C_30min.py',pair,'1'], stdout=f, stderr=ferr)
             f.close()
             ferr.close()
@@ -60,10 +119,43 @@ def runPair(pair):
         	 #f.write(e)
         	 #f.close()
         	 logging.error("something bad happened", exc_info=True)
+ 
+ def runPair_v2(pair):
+    version = 'v2'
+    version_ = 'v2.4C'
+    runDPS = True
+    runData = {'ticker':pair, 'showDist':showDist,'showPDFCDF':showPDFCDF,'showAllCharts':showAllCharts,\
+                'runDPS':runDPS,'saveParams':saveParams,'saveDataSet':saveDataSet,'verbose':verbose,\
+                'scorePath' : scorePath, 'equityStatsSavePath' : equityStatsSavePath,'signalPath' : signalPath,\
+                'dataPath' :dataPath, 'bestParamsPath' :  bestParamsPath, 'chartSavePath' :chartSavePath,\
+                'version':version, 'version_':version_, 'filterName':filterName, 'data_type':data_type,\
+                'barSizeSetting':barSizeSetting}
+                
+    if pair not in livePairs:
+        offlineMode(pair, "Offline Mode: turned off in runsystem", signalPath, version, version_)
+        
+    try:
+            f=open ('/logs/' + version+'_'+pair + 'onBar.log','a')
+            print 'Starting '+version+': ' + pair
+            f.write('Starting '+version+': ' + pair)
+            
+            ferr=open ('/logs/' + version+'_'+pair + 'onBar_err.log','a')
+            ferr.write('Starting '+version+': ' + pair)
+            
+            signal=runv2(runData)
+            subprocess.call(['python','debug_system_v1.3C_30min.py',pair,'1'], stdout=f, stderr=ferr)
+            f.close()
+            ferr.close()
+     except Exception as e:
+        	 #f=open ('./debug/v1run' + pair + '.log','a')
+        	 #f.write(e)
+        	 #f.close()
+        	 logging.error("something bad happened", exc_info=True)
+             
     
 threads = []
-for pair in pairs:
-	sig_thread = threading.Thread(target=get_bars, args=[[pair], '30m_'])
+for pair in livePairs:
+	sig_thread = threading.Thread(target=get_bars, args=[[pair], barSizeSetting+'_'])
 	sig_thread.daemon=True
 	threads.append(sig_thread)
 	sig_thread.start()

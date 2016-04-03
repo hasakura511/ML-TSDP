7c547c1edb04a62f00663ae7ef99732ab356f714
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-04 00:30:30 +0900

    hi

--- sys_alert.py
+++ sys_alert.py
@@ -6,15 +6,7 @@ from os import listdir
 from os.path import isfile, join
 from ibapi.get_feed import get_feed, get_realtimebar,getDataFromIB, get_history, proc_history
 from c2api.place_order import place_order as place_c2order
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 08 20:10:29 2016
-3 mins - 2150 dp per request
-10 mins - 630 datapoints per request
-30 mins - 1025 datapoints per request
-1 hour - 500 datapoint per request
-@author: Hidemi
-"""
+from dateutil.parser import parse
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -33,23 +25,27 @@ import os
 import logging
 import re
 import threading
+import seitoolz.bars as bars
+import sys
+# Import smtplib for the actual sending function
+import smtplib
+# Import the email modules we'll need
+from email.mime.text import MIMEText
 
 logging.basicConfig(filename='/logs/sys_alert.log',level=logging.DEBUG)
-   
+
+intervals = ['30m','1h','10m','1 min']
 def start_proc():
     interval='1h'
-    minDataPoints = 5000
-    exchange='IDEALPRO'
-    secType='CASH'
     
     pairs=bars.get_currencies()
     threads = []
     #bars.get_hist_bars(pairs, interval, minDataPoints, exchange, secType)
     #bars.create_bars(pairs, interval)
-      
-    t1 = threading.Thread(target=check_bar, args=[pairs, interval, minDataPoints, exchange, secType])
-    t1.daemon=True
-    threads.append(t1)
+    for interval in intervals:
+        t1 = threading.Thread(target=check_bar, args=[pairs, interval])
+        t1.daemon=True
+        threads.append(t1)
     
     #t2 = threading.Thread(target=bars.create_bars, args=[pairs, interval])
     #t2.daemon=True
@@ -57,46 +53,67 @@ def start_proc():
     
     [t.start() for t in threads]
     #[t.join() for t in threads]
-    bars.update_bars(pairs, interval)
+    #bars.update_bars(pairs, interval)
 
 def send_alert(msg):
-    print 'Alert'
+    logging.info(msg)
+    print msg
+    try:
+        me='catchall@neospace.com'
+        you='consulting@neospace.com'
+        # Create a text/plain message
+        message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+             """ % (me, ", ".join(you), msg, msg)
+        
+        
+        # Send the message via our own SMTP server, but don't include the
+        # envelope header.
+        server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+        server.ehlo()
+        #server.starttls()
+        server.login('catchall@neospace.com','W0h0h0h0')
+        server.sendmail(me, [you], message)
+        server.quit()
+    except Exception as e:
+        logging.error("send_alert", exc_info=True)
+    
     
 check=dict()
 
-def check_bar(interval, searchstr):
+def check_bar(pairs, interval):
     dataPath = './data/from_IB/'
-    files = [ f for f in listdir(dataPath) if isfile(join(dataPath,f)) ]
-    for file in files:
-        
-        if re.search(r'' + searchstr,file):
-            data=pd.read_csv(dataPath + file, 'Date')
-            if interval == '30m':
-                t1 = threading.Thread(target=start_check, args=[interval, searchstr])
-                t1.daemon=True
-                threads.append(t1)
-                lastDate=str(data.index[-1])
-                if check.has_key(lastDate):
-                    if check[lastDate] > 3:
-                        send_alert(msg)
-                        
-                if not lastDate.has_key(symbol):
-                        lastDate[symbol]=timestamp
-                                               
-                if lastDate[symbol] < timestamp:
-                    returnData=True
-
-def start_check(interval, searchstr):
-    dataPath = './data/from_IB/'
-    files = [ f for f in listdir(dataPath) if isfile(join(dataPath,f)) ]
-    for file in files:
-        
-        if re.search(r'' + searchstr,file):
-            data=pd.read_csv(dataPath + file, 'Date')
-            if interval == '30m':
-                t1 = threading.Thread(target=check_bar, args=[interval, searchstr])
-                t1.daemon=True
-                threads.append(t1)          
-                
-                
-    time.sleep(interval)
+    barPath='./data/bars/'
+    while 1:
+        try:
+            for pair in pairs:
+                dataFile=dataPath + interval + '_' + pair + '.csv'
+                barFile=barPath + interval + '_' + pair + '.csv'
+                if os.path.isfile(dataFile) and os.path.isfile(barFile):
+                    
+                    data=pd.read_csv(dataFile, index_col='Date')
+                    bar=pd.read_csv(barFile, index_col='Date')
+                    eastern=timezone('US/Eastern')
+                    #timestamp
+                    dataDate=parse(data.index[-1]).replace(tzinfo=eastern)
+                    barDate=parse(bar.index[-1]).replace(tzinfo=eastern)
+                    dtimestamp = time.mktime(dataDate.timetuple())
+                    btimestamp = time.mktime(barDate.timetuple())
+                    timestamp=int(time.time())
+                    checktime=30
+                    if interval == '30m':
+                        checktime = 30
+                    elif interval == '1h':
+                        checktime = 70
+                    elif interval == '1 min':
+                        checktime = 3
+                    
+                    if timestamp - dtimestamp > checktime:
+                        send_alert("Feed " + pair + " Interval: " + interval + " Not Updating Since: " + str(data.index[-1]))
+                    if timestamp - btimestamp > checktime:
+                        send_alert("Bar " + pair + " Interval: " + interval + " Not Updating Since: " + str(data.index[-1]))
+                    
+            time.sleep(60)
+        except Exception as e:
+            logging.error("check_bar", exc_info=True)
+            
+send_alert('hi')

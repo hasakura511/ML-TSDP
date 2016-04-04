db53b2a472e2800b0e37b45e000956497158833e
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-04 19:13:11 +0900

    hi

--- t.py
+++ t.py
@@ -2,8 +2,10 @@ import seitoolz.bars as bars
 
 def onBar(bar, symbols):
 	print "==============="
+	print "Date: " + bar['Date'] + '\n'
 	for symbol in symbols:
 		print "OnBar: " + symbol + '\n'
+		print "OnBar: " + bar[symbol] + '\n'
 	print "==============="
 
 bars.get_last_bar(['EURUSD','USDJPY'], 'Close', onBar)

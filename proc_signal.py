6d63ef3b76ba840eae1ac343fe782dc8e5d7d614
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-04 16:11:30 +0900

    hi

--- proc_signal.py
+++ proc_signal.py
@@ -42,18 +42,21 @@ subprocess.call(['python', 'get_ibpos.py'])
 ib_pos=get_ibpos_from_csv()
 
 for i in systemdata.index:
-   system=systemdata.ix[i]
-   model=model_pos
-   if system['ibsym'] != 'BTC':
-        if system['Version'] == 'v1':
-                model=model_pos
-        else:
-                model=dps_model_pos
-        
-        adj_size(model, ib_pos,system['System'], system['Name'], str(system['c2id']),system['c2api'],system['c2qty'],
-                 system['c2sym'],system['c2type'],system['c2submit'], system['ibqty'],system['ibsym'],system['ibcur'],
-                 system['ibexch'],system['ibtype'],system['ibsubmit'])
-        time.sleep(1)
+    try:
+       system=systemdata.ix[i]
+       model=model_pos
+       if system['ibsym'] != 'BTC':
+            if system['Version'] == 'v1':
+                    model=model_pos
+            else:
+                    model=dps_model_pos
+            if system['c2submit'] or system['ibsubmit']:
+                adj_size(model, ib_pos,system['System'], system['Name'], str(system['c2id']),system['c2api'],system['c2qty'],
+                         system['c2sym'],system['c2type'],system['c2submit'], system['ibqty'],system['ibsym'],system['ibcur'],
+                         system['ibexch'],system['ibtype'],system['ibsubmit'])
+                time.sleep(1)
+    except Exception as e:
+        logging.error("something bad happened", exc_info=True)
 #subprocess.call(['python', 'proc_signal_v2.py'])
 #subprocess.call(['python', 'get_ibpos.py'])
 #subprocess.call(['python', 'proc_signal_v2dps.py'])

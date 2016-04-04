b5d6a608db9cc13f68122efc98438fee33a292e2
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-04 18:38:51 +0900

    hi

--- runsystem_cron_5min.bat
+++ runsystem_cron_5min.bat
@@ -1,5 +1,5 @@
 cd \cygwin64\media\sf_python\tsdp
 
-\anaconda2\python get_ibpos.py
+rem \anaconda2\python get_ibpos.py
 \anaconda2\python proc_signal.py
-\anaconda2\python get_ibpos.py
+rem \anaconda2\python get_ibpos.py

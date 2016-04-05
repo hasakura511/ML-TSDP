a8b58cd917cf0e7e803279274e2d2b972c73c2b4
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-06 05:41:07 +0900

    hi

--- runsystem_cron_daily.bat
+++ runsystem_cron_daily.bat
@@ -1,4 +1,4 @@
 cd \cygwin64\media\sf_python\tsdp
 
-\anaconda2\python get_exec.py
-\anaconda2\python get_ibpnl.py
+\anaconda2\python get_exec.py   >>  \logs\get_exec.log
+\anaconda2\python get_ibpnl.py  >>  \logs\get_ibpnl.log

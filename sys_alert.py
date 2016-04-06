5ae11532594c81d34574c5071733a67625e5d997
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-06 10:31:01 +0900

    hi

--- sys_alert.py
+++ sys_alert.py
@@ -153,6 +153,10 @@ def check_bar(pairs, interval, tradingHours=True):
                         checktime = 10
                         if re.search(r'local', pair) or re.search(r'kraken', pair):
                             checktime = 1440
+                        if re.search(r'itb', pair) or re.search(r'coin', pair):
+                            checktime = 1440
+                        if re.search(r'curex', pair) or re.search(r'konan', pair):
+                            checktime = 1440
                             
                     checktime = checktime * 60
                                         

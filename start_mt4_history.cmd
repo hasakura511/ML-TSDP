04fc7796ff9789224a16322985dff786cb9bad5c
yfushimi@neospace.com <yfushimi@neospace.com>
2016-04-18 07:33:45 +0900

    hi

--- start_mt4_history.cmd
+++ start_mt4_history.cmd
@@ -1,3 +1,7 @@
 cd \cygwin64\media\sf_python\tsdp
+del data\from_mt4\LOG*.*
+rm data/from_mt4/*/LOG*
+del c:\mql4\files\LOG*.*
+rm /cygdrive/c/mql4/files/*/LOG*
 xcopy /y /s /c /z C:\mql4\files\*.* data\from_mt4\
 exit

734d03dbb26f81885818b49259ace4b5dd04707f
hasakura511 <hidemi@gmail.com>
2016-05-08 12:07:09 +0900

    data

--- data/ratioAdjust.py
+++ data/ratioAdjust.py
@@ -9,8 +9,22 @@ import numpy as np
 import pandas as pd
 from os import listdir
 from os.path import isfile, join
-
-
+import io
+import traceback
+import json
+import imp
+import urllib
+import urllib2
+import webbrowser
+import re
+import datetime
+import time
+import inspect
+import os
+import os.path
+import sys
+import ssl
+from copy import deepcopy
 
         
 def ratioAdjust(data):
@@ -35,15 +49,72 @@ def ratioAdjust(data):
     #data.to_csv('C:/users/hidemi/desktop/python/debug.csv')
     return pd.concat([data[['DATE','RO', 'RH', 'RL', 'RC']],data[[' VOL', ' OI', ' P', ' R',' RINFO']]],axis=1)
     
+refresh=False
 dataPath='./tickerData/'
-files = [ f for f in listdir(dataPath) if isfile(join(dataPath,f)) ]
+with open('./futures.txt') as f:
+    marketList = f.read().splitlines()
+    
+nMarkets = len(marketList)
 
-for i,f in enumerate(files):
-    if 'F_' in f:
-        print 'Adjusting',f,i
-        #if 'F_'+contract+'.txt' in files and (ticker[0:3] in contract or ticker[3:6] in contract):
-        data = pd.read_csv(dataPath+f)
-        #data.index.name = 'Dates'
-        data=ratioAdjust(data)
-        data.columns = ['Date','Open','High','Low','Close','Volume','OI','P','R','RINFO']
-        data.to_csv(dataPath+f, index=False)
+if sys.version[:5] is '2.7.9':
+    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+
+# set up data director
+if not os.path.isdir(dataPath):
+    os.mkdir(dataPath)
+    refresh=True
+    
+if sys.version[:5] is '2.7.9':
+    data = urllib.urlopen('https://www.quantiacs.com/data/F_' +
+                          marketList[0]+'.txt',
+                          context=gcontext).read()
+    newdata = pd.read_csv(io.StringIO(data.decode('utf-8'))).iloc[-1]
+    olddata = pd.read_csv(dataPath+'F_'+marketList[0]+'.txt').iloc[-1]
+    if newdata.DATE != olddata.Date:
+        refresh=True
+else:
+    data = urllib.urlopen('https://www.quantiacs.com/data/F_' +
+                          marketList[0]+'.txt').read()
+    newdata = pd.read_csv(io.StringIO(data.decode('utf-8'))).iloc[-1]
+    olddata = pd.read_csv(dataPath+'F_'+marketList[0]+'.txt').iloc[-1]
+    if newdata.DATE != olddata.Date:
+        refresh=True
+
+if refresh:
+    print 'Refreshing Data..'
+    for j in range(nMarkets):
+        path = os.path.join(dataPath, +'F_'+marketList[j]+'.txt')
+
+        # check to see if market data is present. If not (or refresh is true), download data from quantiacs.
+
+        try:
+            if sys.version[:5] is '2.7.9':
+                data = urllib.urlopen('https://www.quantiacs.com/data/F_' +
+                                      marketList[j]+'.txt',
+                                      context=gcontext).read()
+            else:
+                data = urllib.urlopen('https://www.quantiacs.com/data/F_' +
+                                      marketList[j]+'.txt').read()
+
+            with open(path, 'w') as dataFile:
+                dataFile.write(data)
+            print 'Downloading ' + marketList[j]
+
+        except:
+            print 'Unable to download ' + marketList[j]
+            marketList.remove(marketList[j])
+            
+
+    files = [ f for f in listdir(dataPath) if isfile(join(dataPath,f)) ]
+
+    for i,f in enumerate(files):
+        if 'F_' in f:
+            print 'Ratio Adjusting',f,i
+            #if 'F_'+contract+'.txt' in files and (ticker[0:3] in contract or ticker[3:6] in contract):
+            data = pd.read_csv(dataPath+f)
+            #data.index.name = 'Dates'
+            data=ratioAdjust(data)
+            data.columns = ['Date','Open','High','Low','Close','Volume','OI','P','R','RINFO']
+            data.to_csv(dataPath+f, index=False)
+else:
+    print 'No New Data Found.'
--- download_quantiacs.bat
+++ download_quantiacs.bat
@@ -1,5 +1,5 @@
-cd \cygwin64\media\sf_python\tsdp\data\tickerData
-echo Y | del *.*
+rem cd \cygwin64\media\sf_python\tsdp\data\tickerData
+rem echo Y | del *.*
 cd \cygwin64\media\sf_python\tsdp\data
-\anaconda2\python rundata.py >> \logs\quantiacs_data.log
+rem \anaconda2\python rundata.py >> \logs\quantiacs_data.log
 \anaconda2\python ratioAdjust.py >> \logs\quantiacs_data.log

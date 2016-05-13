6618a96e8429b2dc389f48b189e2adc10f652be5
yuukifushimi <yuukifushimi@gmail.com>
2016-05-13 03:03:14 -0400

    data

--- download_quantiacs.bat
+++ download_quantiacs.bat
@@ -5,3 +5,4 @@ rem \anaconda2\python rundata.py >> \logs\quantiacs_data.log
 \anaconda2\python ratioAdjust2.py %1 >> \logs\quantiacs_data.log
 cd \cygwin64\media\sf_python\tsdp
 \anaconda2\python heatmap_futures.py 1 >> \logs\quantiacs_data.log
+\anaconda2\python heatmap_currencies2.py 1 >> \logs\quantiacs_data.log
---
+++ start_system_v4C.bat
@@ -0,0 +1,2 @@
+cd \cygwin64\media\sf_python\tsdp
+\anaconda2\python run_currencies.py %1 %2 %3>> \logs\v4_%1.log

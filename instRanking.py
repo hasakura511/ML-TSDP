2a70ec8fc73533bac4de64d9d20cb059dd3f9218
hasakura511 <hidemi@gmail.com>
2016-07-06 10:43:26 +0900

    data

--- instRanking.py
+++ instRanking.py
@@ -1,3 +1,4 @@
+import time
 import os
 import math
 import string
@@ -23,6 +24,8 @@ from suztoolz.transform import RSI, ROC, zScore, softmax, DPO, numberZeros,\
                         roofingFilter
 import seaborn as sns
 
+filter=False
+start_time = time.time()
 version = 'v4'
 groups  = {
         'AC':'energy',
@@ -105,13 +108,16 @@ groups  = {
         'YT2':'rates',
         'YT3':'rates',
         }
-        
+systemFilename='system_v4futures.csv'
+
 if len(sys.argv) > 1:
     #bestParamsPath = './data/params/'
     #signalPath = './data/signals/'
     #dataPath = './data/csidata/v4futures2/'
     equityCurveSavePath = './data/signalPlots/'
     savePath= './data/results/'
+    atrPath='./data/futuresATR.csv'
+    systemPath =  './data/systems/'
     #pngPath = './data/results/'
     #showPlot = False
     #verbose = False
@@ -123,32 +129,67 @@ else:
     #bestParamsPath = 'C:/Users/Hidemi/Desktop/Python/SharedTSDP/data/params/' 
     equityCurveSavePath = 'C:/Users/Hidemi/Desktop/Python/SharedTSDP/data/signalPlots/' 
     savePath= 'C:/Users/Hidemi/Desktop/Python/SharedTSDP/data/signalPlots/' 
+    atrPath='D:/ML-TSDP/data/futuresATR.csv'
+    systemPath =  'C:/Users/Hidemi/Desktop/Python/SharedTSDP/data/systems/' 
     #pngPath = 'C:/Users/Hidemi/Desktop/Python/SharedTSDP/data/signalPlots/' 
     #showPlot = True
     #verbose = True
-
+    
+#load symbol names less than 6 chars long
 dataFiles = [ f for f in listdir(equityCurveSavePath) if \
                     isfile(join(equityCurveSavePath,f)) and '.csv' in f and version+'_' in f and\
                     len(f.split('_')[1].split('.')[0])<6]
 print len(dataFiles), dataFiles
 
+
+atrFile = pd.read_csv(atrPath, index_col=0)
+
 futuresRank=pd.DataFrame()
 for f in dataFiles:
-    lookback=10
+    
     sym = f.split('_')[1].split('.')[0]
-    df = pd.read_csv(equityCurveSavePath+f, index_col=0)
-    df.index.name='dates'
-    #print df
-    nrows=df.shape[0]
-    if nrows <lookback:
-        lookback= nrows
-        
-    pctChg=(df.equity[-1]-df.equity[-lookback])/df.equity[-lookback]
-    futuresRank.set_value(sym,'G/L Last '+str(lookback),pctChg)
-    futuresRank.set_value(sym,'group',groups[sym])
-    futuresRank.set_value(sym,str(df.index[-lookback])+' to '+str(df.index[-1]),pctChg)
+    if sym in groups:
+        #+1 to make the minimum index -2
+        lookback=int(abs(atrFile.ix[sym].LastSRUN))+1
+        print sym,lookback,
+        df = pd.read_csv(equityCurveSavePath+f, index_col=0)
+        df.index.name='dates'
+        #print df
+        nrows=df.shape[0]
+        if nrows <lookback:
+            lookback= nrows
+            
+        pctChg=(df.equity[-1]-df.equity[-lookback])/df.equity[-lookback]
+        futuresRank.set_value(sym,'G/L Last',pctChg)
+        futuresRank.set_value(sym,'group',groups[sym])
+        futuresRank.set_value(sym,'lookback',lookback)
+        futuresRank.set_value(sym,'LastDate',df.index[-1])
+        futuresRank.set_value(sym,str(lookback)+' '+str(df.index[-lookback])+' to '+str(df.index[-1]),pctChg)
 futuresRank.index.name='Symbol'
 futuresRank = futuresRank.sort_values(by=futuresRank.columns[0], ascending=False).reset_index()
-print 'Saving', savePath+'fRank.html'
+print '\nSaving', savePath+'fRank.html'
 futuresRank.to_html(savePath+'fRank.html')
-print futuresRank
+print futuresRank.ix[:,0:5]
+
+if filter:
+    #filter off un/profitable contracts
+    print '\nOffline:'
+    offlineSymbols = sorted(futuresRank[futuresRank['G/L Last']<0].Symbol.values)
+    system = pd.read_csv(systemPath+systemFilename)
+    print offlineSymbols
+    for sys in system.System:
+        sym=sys.split('_')[1]
+        #print sym
+        if sym in offlineSymbols:
+            idx=system[system.System==sys].index[0]
+            print sys, sym, system.ix[idx].c2qty,
+            system.set_value(idx,'c2qty',0)
+            print system.ix[idx].c2qty
+            
+    print 'Saving', systemPath+systemFilename
+    system.to_csv(systemPath+systemFilename, index=False)
+        
+#signalDF=signalDF.sort_index()
+#print signalDF
+#signalDF.to_csv(savePath+'futuresSignals.csv')
+print 'Elapsed time: ', round(((time.time() - start_time)/60),2), ' minutes ', dt.now()

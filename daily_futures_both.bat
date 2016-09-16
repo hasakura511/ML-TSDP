86d23c3030653541c38162afca072769bb095115
yuukifushimi <yuukifushimi@gmail.com>
2016-09-16 02:04:31 -0400

    data

--- daily_futures_both.bat
+++ daily_futures_both.bat
@@ -1,6 +1,6 @@
 cd \cygwin64\media\sf_python\tsdp
 rem \anaconda2\python vol_adjsize.py 1 >> \logs\daily_futures.log
-copy .\data\systems\system.csv .\data\systems\system_backup.csv
+rem copy .\data\systems\system.csv .\data\systems\system_backup.csv
 \anaconda2\python proc_sig_adj.py >> \logs\daily_futures_orders.log
 copy .\data\systems\system_v4futures.csv .\data\systems\system.csv
 \anaconda2\python proc_signal_v4.py 1 v4futures >> \logs\daily_futures_orders.log

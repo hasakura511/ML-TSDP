7f125609de4d9234337d61637e60df186f0f4d6c
hasakura511 <hidemi@gmail.com>
2016-10-07 08:10:51 +0800

    data

---
+++ check_systems.py
@@ -0,0 +1,69 @@
+import requests
+import os
+import numpy as np
+import pandas as pd
+import subprocess
+import numpy as np
+import pandas as pd
+import time
+#from ibapi.place_order import place_order as place_iborder
+#from c2api.place_order import place_order as place_c2order
+import json
+from pandas.io.json import json_normalize
+from ibapi.get_exec import get_ibpos, get_exec_open as get_ibexec_open, get_ibpos_from_csv
+from c2api.get_exec import get_exec_open, get_c2_list, get_c2livepos
+#from seitoolz.signal import get_dps_model_pos, get_model_pos
+#from seitoolz.order import adj_size
+#from seitoolz.get_exec import get_executions
+from time import gmtime, strftime, localtime, sleep
+import logging
+import sys
+import threading
+from datetime import datetime as dt
+
+#logging.basicConfig(filename='/logs/refresh_c2.log',level=logging.DEBUG)
+start_time = time.time()
+systems = ['v4futures','v4mini','v4micro']
+
+if len(sys.argv)==1:
+    savePath='D:/ML-TSDP/data/portfolio/'
+    systemPath = 'D:/ML-TSDP/data/systems/'
+else:
+    savePath='./data/portfolio/'
+    systemPath =  './data/systems/'
+
+c2dict={}
+futuresDict={}
+for sys in systems:
+    #subprocess.call(['python', 'get_ibpos.py'])       
+    systemdata=pd.read_csv(systemPath+'system_'+sys+'.csv')
+    futuresDict[sys]=systemdata=systemdata.reset_index()
+    futuresDict[sys].index=futuresDict[sys].c2sym
+    #portfolio and equity
+    c2list=get_c2_list(systemdata)
+    systems=c2list.keys()
+    for systemname in systems:
+        (systemid, apikey)=c2list[systemname]
+        c2dict[systemname]=get_c2livepos(systemid, apikey, systemname)
+        
+    #trades
+    #get_executions(systemdata)
+    #subprocess.call(['python', 'get_ibpos.py'])
+
+for sys in c2dict.keys():
+    print sys, 'Position Checking..'
+    #sig reconciliation
+    c2dict[sys]['signal']=np.where(c2dict[sys]['long_or_short'].values=='long',1,-1)
+    for sym in c2dict[sys].index:
+        if sym in futuresDict[sys].index:
+            c2sig = int(c2dict[sys].ix[sym].signal)
+            sig=int(futuresDict[sys].ix[sym].signal)
+            qty=int(futuresDict[sys].ix[sym].c2qty)
+            c2qty=int(c2dict[sys].ix[sym].quant_opened)
+            if sig != c2sig or qty != c2qty:
+                print 'position mismatch: ', sym, sig, c2sig, qty, c2qty
+        else:
+            print sym, ' not in system file. exiting contract..'
+            #place order to exit the contract.
+
+print 'Elapsed time: ', round(((time.time() - start_time)/60),2), ' minutes ', dt.now()
--- vol_adjsize_c2.py
+++ vol_adjsize_c2.py
@@ -85,7 +85,9 @@ cDictCSI = {
             'USDJPY':'QE90000$.CSV',
             'USDCHF':'QE}0000$.CSV',
             }
-            
+currenciesDF_old=pd.read_csv(savePath+'currenciesATR.csv', index_col=0)
+oldDate=dt.strptime(currenciesDF_old.index.name,"%Y-%m-%d %H:%M:%S")
+
 currenciesDF=pd.DataFrame()
 corrDF=pd.DataFrame()
 for pair in currencyPairs:
@@ -108,58 +110,65 @@ for pair in currencyPairs:
     currenciesDF.set_value(pair,'Close'+str(data.index[-1]),data.Close.iloc[-1])
     currenciesDF.set_value(pair,'PC'+str(data.index[-1]),pc.iloc[-1])
     currenciesDF.set_value(pair,'ACT'+str(data.index[-1]),priorSig)
-currenciesDF.index.name=data.index[-1]
+currenciesDF.index.name=lastDate=data.index[-1]
 #corrDF.to_csv(savePath+'currenciesPCcsv')
 #corrDF.corr().to_csv(savePath+'currenciesCorr.csv')
-corrDF=corrDF.corr()
-fig,ax = plt.subplots(figsize=(13,13))
-ax.set_title('Correlation '+str(data.index[0])+' to '+str(data.index[-1]))
-sns.heatmap(ax=ax,data=corrDF)
-plt.yticks(rotation=0) 
-plt.xticks(rotation=90) 
-corrDF.to_html(savePath2+'currencies_5.html')
 
-if savePath != None:
-    print 'Saving '+savePath2+'currencies_4.png'
-    fig.savefig(savePath2+'currencies_4.png', bbox_inches='tight')
-    
-if len(sys.argv)==1:
-    print data.index[0],'to',data.index[-1]
-    plt.show()
-plt.close()
+if lastDate >oldDate:
+    print 'first time run.. generating new currency ATR file.'
+    corrDF=corrDF.corr()
+    fig,ax = plt.subplots(figsize=(13,13))
+    ax.set_title('Correlation '+str(data.index[0])+' to '+str(data.index[-1]))
+    sns.heatmap(ax=ax,data=corrDF)
+    plt.yticks(rotation=0) 
+    plt.xticks(rotation=90) 
+    corrDF.to_html(savePath2+'currencies_5.html')
 
-for col in corrDF:
-    plt.figure(figsize=(8,10))
-    corrDF[col].sort_values().plot.barh(color='r')
-    plt.axvline(0, color='k')
-    plt.title(col+' '+str(lookback)+' Day Correlation '+str(data.index[0])+' to '+str(data.index[-1]))
-    plt.xlim(-1,1)
-    plt.xticks(np.arange(-1,1.25,.25))
-    plt.grid(True)
-    filename='v4_'+col+'_CORREL'+'.png'
-    if savePath2 != None:
-        print 'Saving '+savePath2+filename
-        plt.savefig(savePath2+filename, bbox_inches='tight')
-    
+    if savePath != None:
+        print 'Saving '+savePath2+'currencies_4.png'
+        fig.savefig(savePath2+'currencies_4.png', bbox_inches='tight')
+        
     if len(sys.argv)==1:
-        #print data.index[0],'to',data.index[-1]
+        print data.index[0],'to',data.index[-1]
         plt.show()
     plt.close()
 
+    for col in corrDF:
+        plt.figure(figsize=(8,10))
+        corrDF[col].sort_values().plot.barh(color='r')
+        plt.axvline(0, color='k')
+        plt.title(col+' '+str(lookback)+' Day Correlation '+str(data.index[0])+' to '+str(data.index[-1]))
+        plt.xlim(-1,1)
+        plt.xticks(np.arange(-1,1.25,.25))
+        plt.grid(True)
+        filename='v4_'+col+'_CORREL'+'.png'
+        if savePath2 != None:
+            print 'Saving '+savePath2+filename
+            plt.savefig(savePath2+filename, bbox_inches='tight')
+        
+        if len(sys.argv)==1:
+            #print data.index[0],'to',data.index[-1]
+            plt.show()
+        plt.close()
 
+
+        
+    for pair in currencyPairs:
+        signalFilename='v4_'+pair+'.csv'
+        #print signalFilename
+        data = pd.read_csv(signalPath+signalFilename, index_col=0)
+        #currenciesDF.set_value(pair,'SIG'+str(data.index[-3]),data.signals.iloc[-3])
+        currenciesDF.set_value(pair,'SIG'+str(data.index[-1]),data.signals.iloc[-1])
+
+    #currenciesDF=currenciesDF.sort_index()
+    print currenciesDF
+    print 'Saving...', savePath+'currenciesATR.csv'
+    currenciesDF.to_csv(savePath+'currenciesATR.csv')
+else:
+    print 'second time run.. skipping job..'
+    pass
     
-for pair in currencyPairs:
-    signalFilename='v4_'+pair+'.csv'
-    #print signalFilename
-    data = pd.read_csv(signalPath+signalFilename, index_col=0)
-    #currenciesDF.set_value(pair,'SIG'+str(data.index[-3]),data.signals.iloc[-3])
-    currenciesDF.set_value(pair,'SIG'+str(data.index[-1]),data.signals.iloc[-1])
-
-#currenciesDF=currenciesDF.sort_index()
-print currenciesDF
-print 'Saving...', savePath+'currenciesATR.csv'
-currenciesDF.to_csv(savePath+'currenciesATR.csv')
-
+    
 #signalDF=signalDF.sort_index()
 #print signalDF
 #signalDF.to_csv(savePath+'currenciesSignals.csv')

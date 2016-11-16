99fc85894049047ccfa878e7f543f82cc0f2d056
yuukifushimi <yuukifushimi@gmail.com>
2016-11-16 06:34:51 -0500

    data

--- clear_csidata4.bat
+++ clear_csidata4.bat
@@ -1,3 +1,4 @@
 set folder="C:\cygwin64\media\sf_python\tsdp\data\csidata\v4futures4"
 cd /d %folder%
-for /F "delims=" %%i in ('dir /b') do (del "%%i" /s/q)
+rem for /F "delims=" %%i in ('dir /b') do (del "%%i" /s/q)
+del . /F /Q

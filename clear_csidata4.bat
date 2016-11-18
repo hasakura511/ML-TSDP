cb23fe0d627d0bcdcac1587ea57241f22e02e489
hasakura511 <hidemi@gmail.com>
2016-11-18 15:21:17 +0800

    data

--- clear_csidata4.bat
+++ clear_csidata4.bat
@@ -2,3 +2,7 @@ set folder="C:\cygwin64\media\sf_python\tsdp\data\csidata\v4futures4"
 cd /d %folder%
 rem for /F "delims=" %%i in ('dir /b') do (del "%%i" /s/q)
 del . /F /Q
+
+cd \cygwin64\media\sf_python\tsdp
+\anaconda2\python vol_adjsize_c2.py 1 >> \logs\daily_data.log
+\anaconda2\python vol_adjsize.py 1 >> \logs\daily_data.log

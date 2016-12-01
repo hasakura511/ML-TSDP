0e39a433bd69ffd0fe4308bcd84e9ec055333284
hasakura511 <hidemi@gmail.com>
2016-12-01 17:16:37 +0800

    data

--- start_moc_live.py
+++ start_moc_live.py
@@ -1,7 +1,8 @@
 from subprocess import Popen, PIPE, check_output, STDOUT
+import datetime
+fulltimestamp=datetime.datetime.now().strftime('%Y%m%d_%H-%M-%S')
 
-
-with open('\logs\moc_live_error.txt', 'a') as e:
+with open('\logs\moc_live_error_'+fulltimestamp+'.txt', 'w') as e:
     #f.flush()
     #e.flush()
     proc = Popen(['python', '\cygwin64\media\sf_python\\tsdp\moc_live.py','1','1','1','0'], stderr=e)

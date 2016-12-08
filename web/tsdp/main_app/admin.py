f2592527929fef10d57f6c557c38e8d99148b899
hasakura511 <hidemi@gmail.com>
2016-12-09 06:23:10 +0800

    data

--- web/tsdp/main_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Dictionary
-from .models import Treasure
-# Register your models here.
-admin.site.register(Dictionary)
-admin.site.register(Treasure)

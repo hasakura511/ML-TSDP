f39aa046a5882aa5eaf31619ea1b37eef4a4129d
Hidemi Asakura <hidemi@gmail.com>
2016-12-31 00:45:00 -0500

    data

--- daily_futures_size.bat
+++ daily_futures_size.bat
@@ -2,6 +2,6 @@ cd \ml-tsdp
 \anaconda2\python vol_adjsize_c2.py 1 >> \logs\daily_data.log
 \anaconda2\python vol_adjsize.py 1 >> \logs\daily_data.log
 \anaconda2\python refresh_c2.py >> \logs\daily_futures.log
-\anaconda2\python slip_report2.py 1 >> \logs\daily_data.log
+\anaconda2\python slip_report_board.py 1 >> \logs\daily_data.log
 \anaconda2\python slip_report_ib.py 1 >> \logs\daily_data.log
 \anaconda2\python heatmap_futuresCSI.py 1 >> \logs\daily_data.log

eb37f4e059b7cecd8927a4efe46525350ec51d5e
Hidemi Asakura <hidemi@gmail.com>
2017-01-09 05:18:49 -0500

    dta

--- clear_csidata4.bat
+++ clear_csidata4.bat
@@ -4,6 +4,6 @@ rem for /F "delims=" %%i in ('dir /b') do (del "%%i" /s/q)
 del . /F /Q
 
 
-rem cd \ML-TSDP\
-rem \anaconda2\python vol_adjsize_c2.py 1 >> \logs\daily_data.log
+cd \ML-TSDP\
+\anaconda2\python create_board_history.py 1 >> \logs\daily_futures.log
 rem \anaconda2\python vol_adjsize.py 1 >> \logs\daily_data.log
---
+++ post_processing.bat
@@ -0,0 +1,3 @@
+cd \ML-TSDP\
+\anaconda2\python create_board_history.py 1 >> \logs\daily_futures.log
+rem \anaconda2\python vol_adjsize.py 1 >> \logs\daily_data.log

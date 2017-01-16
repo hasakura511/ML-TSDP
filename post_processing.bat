34dd17fa4622ed8db95291c690bfc1f517aa8422
Hidemi Asakura <hidemi@gmail.com>
2017-01-16 02:11:14 -0500

    data

--- post_processing.bat
+++ post_processing.bat
@@ -1,3 +1,4 @@
 cd \ML-TSDP\
 \anaconda2\python create_board_history.py 1 >> \logs\daily_futures.log
+\anaconda2\python excel_charts.py 1 >> \logs\daily_futures.log
 rem \anaconda2\python vol_adjsize.py 1 >> \logs\daily_data.log

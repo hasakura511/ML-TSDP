48985a242ac37aa3d7c43c5874b22439424df7cf
Hidemi Asakura <hidemi@gmail.com>
2017-01-26 01:30:06 -0500

    data

--- post_processing.bat
+++ post_processing.bat
@@ -1,14 +1,6 @@
-rem cd \ML-TSDP\
-rem \anaconda2\python post_processing.py >> \logs\daily_futures.log
+cd \ML-TSDP\
+\anaconda2\python post_processing.py >> \logs\daily_futures.log
 
-@echo off
-for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
-set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-
-set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
-set "fullstamp=%YYYY%%MM%%DD%_%HH%-%Min%-%Sec%"
-@echo on
 cd \ml-tsdp\web\tsdp\
 wmic process where "Commandline like '%%manage.py runserver%%' and name like '%%python.exe%%'" call terminate
 \anaconda2\python runserver.py
--- web/tsdp/runserver.py
+++ web/tsdp/runserver.py
@@ -2,11 +2,10 @@ from subprocess import Popen, PIPE, check_output, STDOUT
 import datetime
 fulltimestamp=datetime.datetime.now().strftime('%Y%m%d_%H-%M-%S')
 
-with open('\logs\runserver_' + fulltimestamp + '.txt', 'w') as f:
-    with open('\logs\runserver_error_'+fulltimestamp+'.txt', 'w') as e:
-        #f.flush()
-        #e.flush()
-        proc = Popen(['python', 'manage.py','runserver','0.0.0.0:80','--noreload'],\
-                     stdout=f, stderr=e)
-        #proc.wait()
-        
+#with open('/logs/runserver_' + fulltimestamp + '.txt', 'w') as f:
+with open('/logs/runserver_'+fulltimestamp+'_error.txt', 'w') as e:
+    #f.flush()
+    #e.flush()
+    proc = Popen(['python', 'manage.py','runserver','0.0.0.0:80','--noreload'], stderr=e)
+    #proc.wait()
+    

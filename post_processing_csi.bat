ebc186fddb0d395aa0775e4fa8952d66c10f2be2
Hidemi Asakura <hidemi@gmail.com>
2017-03-16 11:06:10 +0800

    data

--- post_processing_csi.bat
+++ post_processing_csi.bat
@@ -7,9 +7,9 @@ set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%%MM%%DD%_%HH%-%Min%-%Sec%"
 @echo on
 
-cd \ml-tsdp
+copy /Y \ml-tsdp\data\csidata\v4futures2\*.csv \ml-tsdp\data\csidata\v4futures4\
 \anaconda2\python post_processing_csi.py >> \logs\post_processing_csi_%fullstamp%.txt
 
-cd \ml-tsdp\web\tsdp\
-wmic process where "Commandline like '%%manage.py runserver%%' and name like '%%python.exe%%'" call terminate
-\anaconda2\python runserver.py
+rem cd \ml-tsdp\web\tsdp\
+rem wmic process where "Commandline like '%%manage.py runserver%%' and name like '%%python.exe%%'" call terminate
+rem \anaconda2\python runserver.py

f65149eacd1886028d41044294cbc932846022e1
Hidemi Asakura <hidemi@gmail.com>
2017-03-21 06:04:42 +0800

    data

--- moc_live.py
+++ moc_live.py
@@ -934,9 +934,9 @@ def find_triggers(feeddata, contractsDF):
                         threadlist.append((csiRunSym,popenArgs))
                 else:
                     print 'skipping runsystem append_data returned 0, using last saved signals.'
-                        popenArgs = ['python', runPath,csiRunSym,'0']
-                        #popenArgs2 = ['python', runPath2, csiFileSym,'1']
-                        threadlist.append((csiRunSym,popenArgs))
+                    popenArgs = ['python', runPath,csiRunSym,'0']
+                    #popenArgs2 = ['python', runPath2, csiFileSym,'1']
+                    threadlist.append((csiRunSym,popenArgs))
             else:
                 if dayofweek>=5:
                     print 'skipping append.. day of week', days[dayofweek]

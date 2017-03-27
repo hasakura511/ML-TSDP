230c69c32ecb80be547741a914088c6c87f01100
Hidemi Asakura <hidemi@gmail.com>
2017-03-27 10:52:24 +0800

    dat

--- web/tsdp/betting/start_moc.py
+++ web/tsdp/betting/start_moc.py
@@ -60,8 +60,8 @@ def update_chartdb():
 
 def restart_webserver():
     fulltimestamp=datetime.datetime.now().strftime('%Y%m%d_%H-%M-%S')
-    with open('\logs\restart_webserver_' + fulltimestamp + '.txt', 'w') as e:
-        with open('\logs\restart_webserver_error_'+fulltimestamp+'.txt', 'w') as f:
+    with open('\logs\\restart_webserver_' + fulltimestamp + '.txt', 'w') as e:
+        with open('\logs\\restart_webserver_error_'+fulltimestamp+'.txt', 'w') as f:
             print('restarting webserver...')
             proc = Popen(['./stop_webserver.bat'],\
                          cwd='/ml-tsdp/web/tsdp/',stdout=f, stderr=e)

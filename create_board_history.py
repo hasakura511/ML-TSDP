f88ac2e99b39f978c58055b6f7ca2cb1def84b0e
Hidemi Asakura <hidemi@gmail.com>
2017-09-20 14:41:18 +0800

    dat

--- create_board_history.py
+++ create_board_history.py
@@ -546,11 +546,19 @@ def createRankingChart(ranking, account, line, title, filename):
     plt.close()
     
     #pnl text
+    prevdate=sorted(signalsDict2.keys())[-2]
+    prevsig=signalsDict2[prevdate][line].astype(int).copy()
+    prevsig=(signalsDict2[prevdate][line]*quantity).astype(int).copy()
+    prevsig[prevsig == -1] = 'SHORT'
+    prevsig[prevsig == 1] = 'LONG'
+    prevsig[prevsig == 0] = 'OFF'
     pnl=market_pnl_by_date[currentdate]['PNL_'+line].ix[active_symbols[account]].astype(int)
-    pnl.index=[re.sub(r'\(.*?\)', '', futuresDict.ix[sym].Desc) for sym in pnl.index]
     pnl['Total']=pnl.sum()
     pnl.name='{} as of MOC {}'.format(pnl.name,currentdate)
-    text='<br>'+pd.DataFrame(pnl).to_html()
+    prev_pnl=pd.DataFrame({'Signals':prevsig.ix[pnl.index], pnl.name:pnl})
+    prev_pnl.index=[re.sub(r'\(.*?\)', '', futuresDict.ix[sym].Desc) if sym in futuresDict.index else 'Total' for sym in pnl.index ]    
+    text='<br>'+pd.DataFrame(prev_pnl).to_html()
+    
     
     lookback_name=str(lookback)+'Day Lookback'
     text+='<br>'+lookback_name+': '+', '.join([index+' '+str(round(ranking.ix[index].ix[lookback_name],1))+'%' for index in pair])
